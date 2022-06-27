6101897f2d235d77a7b8939dd69fb5577d9262a1
gnaumova <billanda2@gmail.com>
2022-06-27 16:12:04 +0300

    composition

--- components/pl-composition.js
+++ components/pl-composition.js
@@ -7,24 +7,85 @@ export class PlComposition extends PlForm {
             formTitle: {
                 type: String
             },
+            /**
+             * Код раздела
+             */
             unitcode: {
                 type: String
             },
+            /**
+             * Код композиции
+             */
+            сomposition: {
+                type: String,
+                value: 'default'
+            },
+            /**
+             * Код метода показа
+             */
             showMethod: {
                 type: String,
                 value: 'default'
+            },
+            /**
+             * Форма открыта для выбора записи
+             */
+            selectable: {
+                type: Boolean,
+                value: false
+            },
+            /**
+             * Значение первичного ключа метода показа, возвращающего результат
+             */
+            value: {
+                type: String
+            },
+            /**
+             * Значение поля метода показа, возвращающего результат
+             */
+            caption: {
+                type: String
+            },
+            meta: {
+                type: Object,
+                value: () => ({})
             }
         }
     }
 
-    connectedCallback() {
-        super.connectedCallback();
-        this.form = this;
-    }
-
+    // todo: заполнение value и caption из метода показа, возвращающего результат
     static get template() {
         return html`
-            <pl-unit-view unitcode="[[unitcode]]" show-method="[[showMethod]]"></pl-unit-view>
+            <pl-flex-layout fit vertical>
+                <pl-unit-view unitcode="[[unitcode]]" show-method="[[showMethod]]"></pl-unit-view>
+                <pl-dom-if if="[[selectable]]">
+                    <template>
+                        <pl-flex-layout>
+                            <pl-button variant="primary" label="Выбрать" on-click="[[onSelect]]"></pl-button>
+                            <pl-button variant="ghost" label="Отмена" on-click="[[close]]"></pl-button>
+                        </pl-flex-layout>
+                    </template>
+                </pl-dom-if>
+            </pl-flex-layout>
+            <pl-action id="aGetMeta" endpoint="/@nfjs/compositions/api/getCompositionMeta" data="{{meta}}"></pl-action>
 		`;
     }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.$.aGetMeta.execute({
+            unitcode: this.unitcode,
+            composition: this.composition
+        })
+    }
+
+    onSelect() {
+        this.close({
+            value: this.value,
+            caption: this.caption
+        });
+    }
 }
+
+customElements.define('pl-composition', PlComposition);
--- forms/composition/composition.js
+++ forms/composition/composition.js
@@ -3,17 +3,17 @@ import { PlComposition } from "@nfjs/composition/components/pl-composition.js";
 export default class CompositionFrm extends PlComposition {
     static get properties() {
         return {
-            formTitle: {
-                type: String,
-                value: 'Роли'
-            },
             unitcode: {
                 type: String,
                 value: 'nfc.roles'
             },
-            compositionCode: {
+            composition: {
                 type: String,
                 value: 'default'
+            },
+            selectable: {
+                type: Boolean,
+                value: false
             }
         }
     }
